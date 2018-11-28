74f1170542c983c3b3849f25232e76a3e2f75b6f
Ethan Tom <ethantom@Ethans-MacBook-Pro-3.local>
2018-11-28 14:23:00 -0800

    reverted back to old working code. Previous merge conflicted

--- web2py/parameters_8000.py
+++ web2py/parameters_8000.py
@@ -1,1 +1,1 @@
-password="pbkdf2(1000,20,sha512)$bccbe40c1790ea4e$df7ed1a1f893f13c4554569c309ad82ffb14d0b7"
+password="pbkdf2(1000,20,sha512)$8cfef52d94075851$3cf17cd79edfea93cf623c90251a0dddd5bf320b"
