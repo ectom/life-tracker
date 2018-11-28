58410da2e8b8492bd3a9fc699dd2d5ce1249f9b3
Ethan Tom <ethantom@Ethans-MacBook-Pro-3.local>
2018-11-28 14:56:44 -0800

    merge complete. all errors accounted for

--- web2py/parameters_8000.py
+++ web2py/parameters_8000.py
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-password="pbkdf2(1000,20,sha512)$8cfef52d94075851$3cf17cd79edfea93cf623c90251a0dddd5bf320b"
-=======
-password="pbkdf2(1000,20,sha512)$971e63db418621e3$56abe105aea95a480f46b1cebfcdbe7e87edcaf6"
->>>>>>> mergeAttempt
+password="pbkdf2(1000,20,sha512)$bc8a959e43faf3af$7dbffa4e7b210c5d8af8da86638158548fd311e1"
