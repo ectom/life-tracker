4e60ed5ff0ab17fd64ae726196534e736b6b1189
Ethan Tom <ethantom@Ethans-MacBook-Pro-3.local>
2018-12-02 17:42:53 -0800

    updated gitignore file

--- web2py/parameters_8000.py
+++ web2py/parameters_8000.py
@@ -1,1 +1,1 @@
-password="pbkdf2(1000,20,sha512)$851ae24b506030a6$ebbefcfcb9c3ce9553c750bb84856fc78e88fc66"
+password="pbkdf2(1000,20,sha512)$a6e6c4b827d76a8f$58df8732571dc73ff634b4ac36a106ca01fc02c3"
