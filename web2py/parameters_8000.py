23c5e7d0c8629fe9e11837926a2be253ed8782ad
Ethan Tom <etom579@gmail.com>
2018-12-12 03:34:52 -0800

    update commit

--- web2py/parameters_8000.py
+++ web2py/parameters_8000.py
@@ -1,1 +1,1 @@
-password="pbkdf2(1000,20,sha512)$b56bf44c34be111d$5998cbf9b1af9df12d5c3afa1f74ff6a22c53eda"
+password="pbkdf2(1000,20,sha512)$922250228099c2e5$8681fb4ae5af41dffe1fbca1d2bce9ca7353f08c"
