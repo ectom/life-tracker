e636f2c0cbd784fd7282aea8d5520a59c0ef0615
Ethan Tom <etom579@gmail.com>
2018-12-12 03:32:38 -0800

    attempt merge

--- web2py/parameters_8000.py
+++ web2py/parameters_8000.py
@@ -1,1 +1,1 @@
-password="pbkdf2(1000,20,sha512)$a2c99c85a89ca2e9$89db4bbe7ccedc23eedd3ef52e3f58137ca09434"
+password="pbkdf2(1000,20,sha512)$b56bf44c34be111d$5998cbf9b1af9df12d5c3afa1f74ff6a22c53eda"
